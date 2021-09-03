523e892cf54594915caae81f347b89c15b6c3496
Ichii0518 <ichii0518@gmail.com>
2021-09-04 01:52:22 +0900

    インデント修正

--- src/views/Discussion.vue
+++ src/views/Discussion.vue
@@ -56,77 +56,77 @@ export default {
         console.log(posts)
         this.posts = this.sortByTime(this.makeTree(posts));
       })
-  },
-  makeTree(posts) {
-    for (let post of posts){
-      if (typeof post.parentId !== "undefined") {
-        post.parent = posts.find((p) => {
-          return p.id == post.parentId;
-        });
-        post.parent.replys.push(post);
+    },
+    makeTree(posts) {
+      for (let post of posts){
+        if (typeof post.parentId !== "undefined") {
+          post.parent = posts.find((p) => {
+            return p.id == post.parentId;
+          });
+          post.parent.replys.push(post);
+        }
       }
-    }
-    posts = posts.filter((post) => {
-      return typeof post.parentId === "undefined"
-    })
-    return posts
-  },
-  sortByTime(tree) {
-    let self = this
-    tree.sort((a, b) => a.time - b.time);
-    for (let node of tree) {
-      if (node.replys.length != 0){
-        self.sortByTime(node.replys)
+      posts = posts.filter((post) => {
+        return typeof post.parentId === "undefined"
+      })
+      return posts
+    },
+    sortByTime(tree) {
+      let self = this
+      tree.sort((a, b) => a.time - b.time);
+      for (let node of tree) {
+        if (node.replys.length != 0){
+          self.sortByTime(node.replys)
+        }
       }
-    }
-    return tree;
-  },
-  getPostPathById(id){
-    let post = this.getPostById(id)
-    return this.getPostPath(post)
-  },
-  getPostPath(post) {
-    let index = this.getPostIndexById(post.id)
-    if (typeof post.parent === "undefined") {
-      return index       
-    } else {
-      let parentPath = this.getPostPath(post.parent)
-      return parentPath + "/" + index
-    }
-  },
-  getPostIndexById(id) {
-    let self = this
-    let post = self.getPostById(id)
-    // 返信ではないpostの場合そのままのインデックスを取得
-    if (post.parentId === void 0) {
-      return self.posts.findIndex((p) => {
+      return tree;
+    },
+    getPostPathById(id){
+      let post = this.getPostById(id)
+      return this.getPostPath(post)
+    },
+    getPostPath(post) {
+      let index = this.getPostIndexById(post.id)
+      if (typeof post.parent === "undefined") {
+        return index       
+      } else {
+        let parentPath = this.getPostPath(post.parent)
+        return parentPath + "/" + index
+      }
+    },
+    getPostIndexById(id) {
+      let self = this
+      let post = self.getPostById(id)
+      // 返信ではないpostの場合そのままのインデックスを取得
+      if (post.parentId === void 0) {
+        return self.posts.findIndex((p) => {
+          return p == post;
+        }) + 1;
+      }
+      // 返信である場合、親の返信の中でのインデックスを取得する
+      return post.parent.replys.findIndex((p) => {
         return p == post;
       }) + 1;
-    }
-    // 返信である場合、親の返信の中でのインデックスを取得する
-    return post.parent.replys.findIndex((p) => {
-      return p == post;
-    }) + 1;
-  },
-  getPostById(id) {
-    let self = this
-    for (let post of self.posts) {
-      let ret = self.searchPostById(post, id);
-      if (ret !== null) return ret
-    }
-    return null;
-  },
-  searchPostById(currentPost, id) {
-    // そのpostのidが探しているものと一致するならそれを返す
-    if (currentPost.id == id) {
-      return currentPost
-    }
-    // 全ての返信を探す
-    for (let reply of currentPost.replys) {
-      let ret = this.searchPostById(reply, id)
-      if (ret !== null) return ret;
-    }
-    return null;
+    },
+    getPostById(id) {
+      let self = this
+      for (let post of self.posts) {
+        let ret = self.searchPostById(post, id);
+        if (ret !== null) return ret
+      }
+      return null;
+    },
+    searchPostById(currentPost, id) {
+      // そのpostのidが探しているものと一致するならそれを返す
+      if (currentPost.id == id) {
+        return currentPost
+      }
+      // 全ての返信を探す
+      for (let reply of currentPost.replys) {
+        let ret = this.searchPostById(reply, id)
+        if (ret !== null) return ret;
+      }
+      return null;
     }
   },
   created() {
