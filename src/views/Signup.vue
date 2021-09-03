af59aa9d3a951a8728d9b1e9f4d61edd4f0d8829
komadaru <kodamaharunoid@gmail.com>
2021-09-03 17:11:48 +0900

    signup moved to views

---
+++ src/views/Signup.vue
@@ -0,0 +1,47 @@
+<template>
+    <head>
+        <title>Signup Page</title>
+    </head>
+    <body>
+        <h1>Signup Page</h1>
+    <table>
+        <tr><th>Email</th>
+        <td><input type="email" v-model="email"></td></tr>
+        <tr><th>Password</th>
+        <td><input type="password" v-model="password"></td></tr>
+        <tr><th></th><td>
+            <button @click="signup">Sign up</button>
+        </td></tr>
+    </table>
+    </body>
+</template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+    name: 'Signup',
+    data: function() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        signup: function() {
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+                .then(user => {
+                    alert('Create accout: ', user.email)
+                    this.$router.push('/')
+                })
+                .catch(error => {
+                    alert(error.message)
+                })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
