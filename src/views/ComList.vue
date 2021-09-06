f206a229a53b1966fa222d9e3642282368ff53d5
Ichii0518 <ichii0518@gmail.com>
2021-09-06 13:19:23 +0900

    読み込みエラーを解消

--- src/views/ComList.vue
+++ src/views/ComList.vue
@@ -24,7 +24,7 @@
 </template>
 
 <script>
-//import firebase from "firebase/app"
+import firebase from "firebase/app"
 import Topbar from '@/components/layouts/Topbar'
 export default {
   async created() {
